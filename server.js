7e45fe9ed26b7f8903adf864bb7ffcca3c9d4fca
vrde <agranzot@gmail.com>
2015-07-03 11:15:43 +0200

    Add email reporting

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var http = require('http'),
     proxy = require('http-proxy').createProxy(),
-    rules = require('./rules');
+    rules = require('./rules'),
+    nodemailer = require('nodemailer');
+
 
 var RE = 0, TARGET = 1, HOST = 2,
     hostRegExp = /^https?:\/\/(([^:\/?#]*)(?::([0-9]+))?)/,
     port = process.env.PORT || 8080;
+
+
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: process.env.GIANO_EMAIL_USER,
+        pass: process.env.GIANO_EMAIL_PASS
+    }
+});
 
 
 function getHost(url) {
@@ -21,6 +32,41 @@ function createRules(rules) {
 
 rules = createRules(rules);
 
+proxy.on('error', function (err, req, res) {
+    var summary = {
+        'error': err.code,
+        'url': req.url,
+        'method': req.method,
+        'headers': req.headers
+    };
+
+    res.writeHead(500, {
+        'Content-Type': 'text/plain'
+    });
+
+    res.end('Oopsy, something went wrong. We just sent an email to our ' +
+            'development team to report the error.\n' +
+            'Try to reload the page anyway.');
+
+    var mailOptions = {
+        from: 'Giano <team@ascribe.io>', // sender address
+        to: 'alberto@ascribe.io, tim@ascribe.io, dimi@ascribe.io', // list of receivers
+        subject: '💀 something bad happened 💀', // Subject line
+        text: JSON.stringify(summary, null, '  ')
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Message sent: ' + info.response);
+        }
+    });
+
+    console.log(summary);
+});
+
+
 var server = http.createServer(function (req, res) {
     var i = 0, rule;
 
@@ -36,7 +82,7 @@ var server = http.createServer(function (req, res) {
         target: rule[TARGET],
         autoRewrite: true
     });
-})
+});
 
 console.log('Starting Giano on port', port);
 server.listen(port);
